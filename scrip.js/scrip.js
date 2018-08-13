ef6548a0f9639370f64fc6bf7462154838cedf46
Diorobosoumare <diorobo.soumare@ellisprep.com>
2018-08-13 14:45:13 -0400

    scrip

---
+++ scrip.js/scrip.js
@@ -0,0 +1,12 @@
+var num1 = parseInt(propmt('Enter a number'));
+var num2 = parseInt(propmt('Enter a number'));
+var num3 = parseInt(propmt('Enter a number'));
+
+
+if(num1 > num2 && num1 > num3){
+	alert(num1 + 'is the highest number');
+} eles if (num2 > num1 && num2 > num3){
+	alert(num2 + 'is highest number');
+} else {
+	alert(num3 + is highest);
+}
